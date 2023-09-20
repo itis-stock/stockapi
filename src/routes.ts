26078273451d224fcd5485fe95a8e44d5ffb94b8
lldan <safinildan@gmail.com>
2023-09-20 19:43:01 +0300

    _13

--- src/routes.ts
+++ src/routes.ts
@@ -3,5 +3,5 @@ import timetable from './utils/timetable';
 import firebase from './utils/firebase';
 
 export default function routes(app: Express, timetable: timetable, firebase: firebase) {
-  app.get('/healthcheck', (req: Request, res: Response) => res.sendStatus(200));
+  app.get('/check', (req: Request, res: Response) => res.sendStatus(200).send('OK'));
 }
