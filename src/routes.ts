e80e00fb783bb52f5bafe5af5fe1884adbef266f
lldan <safinildan@gmail.com>
2023-09-20 18:40:35 +0300

    _10

--- src/routes.ts
+++ src/routes.ts
@@ -6,10 +6,6 @@ import documents from './dev/documents';
 import test from './dev/test';
 
 export default function routes(app: Express, timetable: timetable, firebase: firebase) {
-  /**
-   * @openapi
-   *
-   */
   app.get('/healthcheck', (req: Request, res: Response) => res.sendStatus(200));
 
   app.get('/timetable', async (req: Request, res: Response) => {
@@ -17,10 +13,27 @@ export default function routes(app: Express, timetable: timetable, firebase: fir
   });
 
   app.get('/exam', async (req: Request, res: Response) => {
-    res.send(await exam());
+    // res.send(await exam());
   });
 
   app.get('/set', async (req: Request, res: Response) => {
-    res.send(await test());
+    const q = ['teachers', 'exams', 'tests', 'docs'];
+    const qq: any[] = [];
+    for (let i = 0; i < q.length; i++) {
+      const start = new Date();
+      const data = await firebase.getdocs(q[i]);
+      const end = new Date();
+      console.log(
+        (end.getTime() - start.getTime()) / 1000 +
+          ' секунд - ' +
+          q[i] +
+          ' - ' +
+          data.length +
+          ' количество элементов',
+      );
+      qq.push(data);
+    }
+
+    res.send(qq);
   });
 }
