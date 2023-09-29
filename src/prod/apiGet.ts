dd705a187cd18fb542b775e5aa22c30db664ac47
lldan <safinildan@gmail.com>
2023-09-29 12:48:36 +0300

    реализуем документацию

--- src/prod/apiGet.ts
+++ src/prod/apiGet.ts
@@ -1,85 +1,29 @@
-import fs from 'fs';
-import path from 'path';
 import { responseType } from '../@types/response';
-type apiType = {
-  title: string;
-  content: apiContentType[];
-};
-type apiContentType = {
-  type: 'text' | 'subtitle' | 'subsubtitle';
-  item: string | apiContentItemType[];
-};
-type apiContentItemType = {
-  text: string;
-  highlight: boolean;
-};
-export default async function apiGet() {
+import firebase from '../utils/firebase';
+
+export default async function apiGet(firebase: firebase) {
   const responseObject: responseType = {
     response: {
       status: 200,
       time: 0,
-      type: 'object',
+      type: 'array',
       data: null,
     },
   };
   const start = new Date();
   try {
-    const readmePath = path.join(__dirname, '../../README.md');
-    const readme = fs.readFileSync(readmePath, 'utf-8').split('\n');
-    const data: apiType[] = [];
-    for (let i = 0; i < readme.length; i++) {
-      if (readme[i]) {
-        if (readme[i].includes('###')) {
-          data[data.length - 1].content.push({
-            type: 'subsubtitle',
-            item: readme[i].replace('###', '').trim(),
-          });
-        } else if (readme[i].includes('##')) {
-          data[data.length - 1].content.push({
-            type: 'subtitle',
-            item: readme[i].replace('##', '').trim(),
-          });
-        } else if (readme[i].includes('#')) {
-          data.push({
-            title: readme[i].replace('#', '').trim(),
-            content: [],
-          });
-        } else {
-          const item = readme[i].split('`');
-          const itemBuffer: apiContentItemType[] = [];
-          for (let j = 0; j < item.length; j++) {
-            if (j % 2 === 0) {
-              if (item[j]) {
-                itemBuffer.push({
-                  text: item[j],
-                  highlight: false,
-                });
-              }
-            } else {
-              if (item[j]) {
-                itemBuffer.push({
-                  text: item[j],
-                  highlight: true,
-                });
-              }
-            }
-          }
-          data[data.length - 1].content.push({
-            type: 'text',
-            item: itemBuffer,
-          });
-        }
-      }
-    }
-    responseObject.response.data = data;
+    const count = await firebase.getCount('documentary');
+    const data = await firebase.get('documentary', String(count));
+    responseObject.response.data = data?.data || null;
+    responseObject.response.count = data?.data.length || 0;
     const end = new Date();
     responseObject.response.time = (end.getTime() - start.getTime()) / 1000;
     return responseObject;
   } catch (err) {
     console.log(err);
-    responseObject.response.status = 33;
+    responseObject.response.status = 16;
     responseObject.response.type = 'error';
-    responseObject.response.errormessage = 'ошибка при чтении README.md';
+    responseObject.response.errormessage = 'ошибка Firebase';
     return responseObject;
   }
 }
