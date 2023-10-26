b81072b3ee8272beefe3ca41e8ea5042256f48fe
lldan <safinildan@gmail.com>
2023-10-26 18:51:40 +0300

    работаем над документацией

--- src/lib/apiUpdate.ts
+++ src/lib/apiUpdate.ts
@@ -1,16 +1,41 @@
 import fs from "fs";
 import matter from "gray-matter";
 export default async function apiUpdate() {
-  const result = "# Дорогая грусть, прости, не вернусь\n";
+  let result =
+    "# Дорогая грусть, прости, не вернусь\n|Order|Name|Changed|Created|\n|---|---|---|---|\n";
   let mas = [];
   const files = fs.readdirSync("./src/api");
   for (let el of files) {
     const data = fs.readFileSync("./src/api/" + el, "utf-8");
     const stat = fs.statSync("./src/api/" + el);
-    console.log(matter(data));
+    const mat = matter(data);
     mas.push({
-      url: "[]()",
+      url:
+        "[" +
+        mat.data.title +
+        "](https://github.com/itis-stock/stockapi/blob/dev-lldan/src/api/" +
+        el +
+        ")",
+      changed: stat.mtime,
+      created: stat.birthtime,
+      order: mat.data.order,
     });
   }
-  return files;
+  mas = mas.sort((a, b) => a.order - b.order);
+
+  for (let el of mas) {
+    result +=
+      "|" +
+      el.order +
+      "|" +
+      el.url +
+      "|" +
+      el.changed +
+      "|" +
+      el.created +
+      "|\n";
+  }
+  fs.writeFileSync("README.md", result);
+
+  return mas;
 }
